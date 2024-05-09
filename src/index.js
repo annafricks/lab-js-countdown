0d04639db5612bd4a56c14ffca487256d04cf7c3
annafricks <annafricks33@gmail.com>
2024-05-09 16:34:32 -0400

    Lab done with alexis, espedro and rachel

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
+const button = document.getElementById("start-btn");
 
+button.addEventListener("click", () => {
+  console.log("Button clicked!")
+  startCountdown();
+});
 
 
 
@@ -16,17 +20,50 @@ function startCountdown() {
   console.log("startCountdown called!");
 
 
-  // Your code goes here ...
+  function countdown() {
+   
+    let count = 10;
+
+    let timerId = setInterval(function() {
+        count--;
+        console.log(count);
+
+        if(count == 0) {
+           clearInterval(timerId);
+            count = 10;
+            console.log("Lift off!");
+            showToast("lift off!");
+        }
+    }, 1000);
 }
 
+
+
+// function countdown() {
+// 	count--;
+// 	document.getElementById("seconds").innerHTML = String( count );
+// 	if (count > 0) {
+// 		setTimeout(countdown, 1000);
+// 	}
+// };
+
+// setTimeout(countdown, 1000);
 
 
 
 // ITERATION 3: Show Toast
 function showToast(message) {
+  const toastElement = document.getElementById("toast");
+  toastElement.classList.add("show");
+  setTimeout(() => {
+  toastElement.classList.remove("show");
+}, 3000);
+}
   console.log("showToast called!");
 
-  // Your code goes here ...
+  countdown();
+}
+
 
 
 
@@ -35,4 +72,5 @@ function showToast(message) {
 
   // Your code goes here ...
 
-}
+
+  
